3c3d2840fb2560f31be3a3e0d4d554d833a09135
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-03-06 17:25:37 -0600

    consulta a la BD con exito

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,11 +1,22 @@
 <?php 
  function obtenerServicio(){
     try {
-        //code...
         //Importar conexion
+        require 'database.php';
+
         //Realizar consulta
+        $sql = "SELECT * FROM servicios;";
+        $consulta = mysqli_query($db, $sql);
+        
+        
         //Mostrar resultado
+        echo '<pre>';
+        var_dump(mysqli_fetch_assoc($consulta));//assoc para un solo registro otros para mas resultados
+        echo '</pre>';
+
+
     } catch (\Throwable $th) {
         var_dump($th);
     }
  }
+ obtenerServicio();
